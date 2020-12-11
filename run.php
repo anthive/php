7b6fca7b47eb7a6b46c6f23dafa2c19c22bb14d8
Stanislav Smirnov <smirnov@dvhab.ru>
2020-12-11 22:00:36 +1000

    Syntax update

--- run.php
+++ run.php
@@ -1,30 +1,27 @@
 <?php
+
 header('Content-Type: application/json');
 
-const ACTIONS = ["stay","move","eat","take","put"];
-const DIRECTIONS = ["up","down","right","left"];
+const ACTIONS = ['stay', 'move', 'eat', 'take', 'put'];
+const DIRECTIONS = ['up', 'down', 'right', 'left'];
 
-$payload = file_get_contents("php://input");
+$payload = file_get_contents('php://input');
 
-//Hive object from request payload
-$hive = json_decode($payload,true);
+// Hive object from request payload
+$hive = json_decode($payload, true);
 
-$orders = array();
-//Loop through ants and give orders
-foreach ($hive["ants"] as $ant){
-    $order = new stdClass();
-    $order->antId = $ant["id"];
-    $order->act = ACTIONS[rand(0,4)];
-    $order->dir = DIRECTIONS[rand(0,3)];
-    array_push($orders,$order);
-}
+// Loop through ants and give orders
+$orders = array_map(fn($ant) => [
+    'antId' => $ant['id'],
+    'act' => ACTIONS[rand(0, 4)],
+    'dir' => DIRECTIONS[rand(0, 3)],
+], $hive['ants']);
 
-// json output format sample:
-// {"orders": [
-//	 {"antId":1,"act":"move","dir":"down"},
-//	 {"antId":17,"act":"load","dir":"up"}
-//	]}
-$response = new stdClass();
-$response->orders = $orders;
-echo json_encode($response);
-?>
+// Json output format sample:
+// {'orders': [
+//	 {'antId':1,'act':'move','dir':'down'},
+//	 {'antId':17,'act':'load','dir':'up'}
+// ]}
+echo json_encode([
+    'orders' => $orders,
+]);
