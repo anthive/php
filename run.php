9d7466d5de3f88868805b901aaf79c0078eac586
Klimonov <klimonov.dev@gmail.com>
2020-12-28 16:09:57 +0300

    update run.php and payload.json

--- run.php
+++ run.php
@@ -1,22 +1,24 @@
 <?php
+// your bot respons should be json object
 header('Content-Type: application/json');
 
+// available actions and directions
 const ACTIONS = ["stay","move","eat","take","put"];
 const DIRECTIONS = ["up","down","right","left"];
 
 $payload = file_get_contents("php://input");
 
-//Hive object from request payload
+// your bot respons should be json object
 $hive = json_decode($payload,true);
 
 $orders = array();
-//Loop through ants and give orders
+// loop through ants and give orders
 foreach ($hive["ants"] as $ant){
     $order = new stdClass();
     $order->antId = $ant["id"];
-    $order->act = ACTIONS[rand(0,4)];
-    $order->dir = DIRECTIONS[rand(0,3)];
-    array_push($orders,$order);
+    $order->act = ACTIONS[rand(0,4)]; // pick random action from array on line 6
+    $order->dir = DIRECTIONS[rand(0,3)]; // pick random direction from array on line 7
+    array_push($orders,$order); // add order to your response object from line 14
 }
 
 // json output format sample:
@@ -27,4 +29,11 @@ foreach ($hive["ants"] as $ant){
 $response = new stdClass();
 $response->orders = $orders;
 echo json_encode($response);
+
+// this code available at https://github.com/anthive/php
+// to test it localy, submit post request with payload.json using postman or curl
+// curl -X 'POST' -d @payload.json http://localhost:7070
+
+// have fun!
+
 ?>
