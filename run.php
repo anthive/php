4c45dd1fbb9df1a858ced0e8341b2168878efc99
Vitaly Kezlya <vitaly@kezlya.com>
2021-01-01 22:06:34 +0300

    Adding more comments

--- run.php
+++ run.php
@@ -1,27 +1,45 @@
 <?php
-
+// your bot response should be json object
 header('Content-Type: application/json');
 
+// available actions and directions
 const ACTIONS = ['stay', 'move', 'eat', 'take', 'put'];
 const DIRECTIONS = ['up', 'down', 'right', 'left'];
 
+// Sim will make http post request to your bot
+// with json payload that contains information
+// about map and ants
 $payload = file_get_contents('php://input');
 
 // Hive object from request payload
 $hive = json_decode($payload, true);
 
-// Loop through ants and give orders
-$orders = array_map(fn($ant) => [
-    'antId' => $ant['id'],
-    'act' => ACTIONS[rand(0, 4)],
-    'dir' => DIRECTIONS[rand(0, 3)],
-], $hive['ants']);
+$orders = array();
 
-// Json output format sample:
+// Loop through ants and give orders
+// For each enat create order object (move to random direction)
+foreach ($hive['ants'] as $ant){
+    $order->antId = $ant['id'];
+    $order->act = 'move';
+    // pick random direction from array on line 6
+    $order->dir = DIRECTIONS[rand(0,3)];
+    array_push($orders,$order);
+}
+
+// response json should look something like this
 // {'orders': [
 //	 {'antId':1,'act':'move','dir':'down'},
 //	 {'antId':17,'act':'load','dir':'up'}
 // ]}
-echo json_encode([
-    'orders' => $orders,
-]);
+
+// finish your json response 
+$response->orders = $orders;
+echo json_encode($response);
+
+// this code available at https://github.com/anthive/php
+// to test it localy, submit post request with payload.json using postman or curl
+// curl -X 'POST' -d @payload.json http://localhost:7070
+
+// have fun!
+
+?>
